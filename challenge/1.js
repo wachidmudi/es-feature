5c85ea08cde588876e5995ce8a41269962b3dc4c
wachidmudi <wachidmudi@gmail.com>
2020-09-14 11:23:28 +0700

    Add challenge

---
+++ challenge/1.js
@@ -0,0 +1,21 @@
+const students = [ ['John', 1], ['Alex', 2], ['Peter', 1] ]
+
+/*
+[
+  {
+    id: 1,
+    name: 'John',
+    class: 'Science'
+  },
+  {
+    id: 2,
+    name: 'Alex',
+    class: 'English'
+  },
+  {
+    id: 3,
+    name: 'Peter',
+    class: 'Science'
+  }
+]
+ */
---
+++ challenge/2.js
@@ -0,0 +1,82 @@
+const students = [
+  {
+    id: 1,
+    name: 'Tony',
+    class: 'Science',
+    hobbies: ['music', 'food', 'work']
+  },
+  {
+    id: 2,
+    name: 'Steeve',
+    class: 'Sport',
+    hobbies: ['game', 'gym']
+  },
+  {
+    id: 3,
+    name: 'Natasha',
+    class: 'English',
+    hobbies: ['game', 'cook', 'food']
+  },
+  {
+    id: 4,
+    name: 'Bruce',
+    class: 'English',
+    hobbies: ['food', 'biking']
+  }
+]
+
+/*
+  ## Task 1
+[
+  {
+    id: 3,
+    name: 'Natasha',
+    class: 'English',
+    hobbies: ['game', 'cook', 'food']
+  },
+  {
+    id: 4,
+    name: 'Bruce',
+    class: 'English',
+    hobbies: ['food', 'biking']
+  }
+]
+ */
+
+/*
+  ## Task 2
+  {
+    id: 4,
+    name: 'Bruce',
+    class: 'English',
+    hobbies: ['food', 'biking']
+  }
+ */
+
+/*
+  ## Task 3
+[
+  {
+    id: 2,
+    name: 'Steeve',
+    class: 'Sport',
+    hobbies: ['game', 'gym']
+  },
+  {
+    id: 3,
+    name: 'Natasha',
+    class: 'English',
+    hobbies: ['game', 'cook', 'food']
+  }
+]
+ */
+
+/*
+  ## Task 4
+  {
+    1: 'Tony',
+    2: 'Steeve',
+    3: 'Natasha',
+    4: 'Bruce'
+  }
+ */
